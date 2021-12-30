5f9e2cf4a28d7fa7094a4b5479a8bb5060d4a589
Верещагина Ирина Александровна <hjr462@mail.ru>
2021-12-30 18:04:47 +0700

    ind

---
+++ pattLab/IndirectionPatt/Indirection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattLab.IndirectionPatt
+{
+    class Indirection
+    {
+        MeasuringDevice indirectionDevice;
+        public Indirection(MeasuringDevice ind)
+        {
+            indirectionDevice = ind;
+        }
+        public void getMeasurements()
+        {
+            indirectionDevice.getMeasurement();
+        }
+
+    }
+}
